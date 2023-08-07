94d7b72604851df7036776a9fef9f1d7a8df0375
Joshua Chisolm <jkchisolm22@gmail.com>
2023-08-07 23:27:14 +0200

    Introduce EF & Initial migration

---
+++ TwitterAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TwitterAPI.Models;
+
+namespace TwitterAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to postgres
+        options.UseNpgsql(_configuration.GetConnectionString("WebApiDatabase"));
+    }
+    
+    public DbSet<Post> Posts { get; set; }
+}
