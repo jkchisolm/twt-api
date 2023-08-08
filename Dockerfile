04c15a6243792348ae5ce561a1a68b9465d96047
Joshua Chisolm <jkchisolm22@gmail.com>
2023-08-08 09:26:05 +0200

    build: Dockerize application

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["TwitterAPI/TwitterAPI.csproj", "TwitterAPI/"]
+RUN dotnet restore "TwitterAPI/TwitterAPI.csproj"
+COPY . .
+WORKDIR "/src/TwitterAPI"
+RUN dotnet build "TwitterAPI.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "TwitterAPI.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "TwitterAPI.dll"]
--- TwitterAPI/Program.cs
+++ TwitterAPI/Program.cs
@@ -16,7 +16,8 @@ builder.Services.AddScoped<IPostRepository, PostRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(conn));
+builder.Services.AddDbContext<AppDbContext>();
+
 
 var app = builder.Build();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
@@ -33,5 +34,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Migrate the latest changes to the database during startup
+using (var scope = app.Services.CreateScope())
+{
+    Console.WriteLine(conn);
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    if (db.Database.GetPendingMigrations().Any())
+    {
+        db.Database.Migrate();
+    }
+}
 
 await app.RunAsync();
